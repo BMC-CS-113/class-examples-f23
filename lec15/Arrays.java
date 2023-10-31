60f9ff26ae2f1de576d2bc00276a93dcaf507bd8
Adam Poliak <Adam Poliak>
2023-10-31 14:18:24 -0400

    lec 15

--- lec15/Arrays.java
+++ lec15/Arrays.java
@@ -1,12 +1,31 @@
 public class Arrays {
 
-  public static int[] ExpandArray(int[] array) {
-    return null;
+  /*
+  Double the size of the input array
+  and return the new array but make sure
+  all original items remain in their original
+  location
+  */
+  public static int[] expandArray(int[] array) {
+    // 1. create the new array
+    int[] newArray = new int[array.length * 2];
+    // 2. copy elements from old to new
+    for (int idx = 0; idx < array.length; idx++) {
+      newArray[idx] = array[idx];
+    }
+    return newArray;
   }
 
   public static void main(String[] args) {
 
     int[] arr = {1,2,3,4};
+    int[] newArr = expandArray(arr);
+
+    int i = 0;
+    while (i < newArr.length) {
+      System.out.printf("%d: %d\n" , i, newArr[i]);
+      i++;
+    }
 
   }
 }
--- lec15/Bank.java
+++ lec15/Bank.java
@@ -1,18 +1,27 @@
 public class Bank {
 
-   int size;
-   String name;
-   String[] clients;
-   double[] accounts;
+    //instance variables specify
+    // the types of data stored in our
+    // custom data type, aka Class
+    double[] accounts;
+    String[] clients;
+    String address;
+    String name;
 
-   public Bank(String bankName, int numClients) {
-     name = bankName;
-     size = numClients;
-     clients = new String[size];
-     accounts = new double[size];
-   }
+    //constructor
+    public Bank(int size, String location, String bankName) {
+       accounts = new double[size];
+       clients = new String[size];
+       address = location;
+       name = bankName;
+    }
 
-   public String getName() {
-     return name;
-   }
+    public String getName() {
+      return name;
+    }
+
+    public static void main(String[] args) {
+      Bank myBank = new Bank(10, "101 N. Merion Rd", "BMC-CS113");
+      System.out.println("My bank is " + myBank.getName());
+    }
 }
