03306bb6d432f42d164b2bc3858f2a7ee84e7425
Adam Poliak <Adam Poliak>
2023-10-31 12:24:57 -0400

    lec15 starter

---
+++ lec15/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+
+  public static int[] ExpandArray(int[] array) {
+    return null;
+  }
+
+  public static void main(String[] args) {
+
+    int[] arr = {1,2,3,4};
+
+  }
+}
